9cc37c63791ef88510cb6a4a754fef92a7a08a42
sethshaw <sethshaw160@gmail.com>
2024-06-11 16:03:23 +0100

    "Initialising repository"

---
+++ alien_invasion.py
@@ -0,0 +1,30 @@
+import sys
+import pygame
+from settings import Settings
+
+class AlienInvasion:
+    """Overall class to generate game"""
+    
+    def __init__(self):
+        pygame.init()
+        self.settings = Settings()
+        
+        self.clock = pygame.time.Clock()
+        self.screen = pygame.display.set_mode(
+            (self.settings.screen_width, self.settings.screen_height))
+        pygame.display.set_caption("Alien Invasion")
+        
+    def run_game(self):
+        while True:
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    sys.exit
+                    
+            self.screen.fill(self.settings.bg_colour)
+            
+            pygame.display.flip()
+            self.clock.tick(60)
+
+if __name__ == '__main__':
+    ai = AlienInvasion()
+    ai.run_game()
